3772663a5916c7ed31e8dc216c8bd3f5ad793bd5
Nhat <nhatanh1nguyen@gmail.com>
2021-10-18 22:52:31 +0200

    page resolutions

--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -11,14 +11,17 @@ export class IssuesComponent implements OnInit {
     {
       title: 'Total Issues',
       number: 752,
+      
     },
     {
       title: 'Issues Solved',
       number: 295,
+     
     },
     {
       title: 'Total Contributions',
       number: 128,
+    
     },
   ];
 
@@ -27,31 +30,37 @@ export class IssuesComponent implements OnInit {
       number: 1,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
     { 
       number: 2,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
     { 
       number: 3,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
     { 
       number: 4,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
     { 
       number: 5,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
     { 
       number: 6,
       date: '01/10/21',
       description: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
     },
   ]
   constructor() { }
--- src/app/resolutions/resolutions.component.ts
+++ src/app/resolutions/resolutions.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resolutions.component.scss']
 })
 export class ResolutionsComponent implements OnInit {
-
+  RESOLUTION = [
+    { 
+      number: 1,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+    { 
+      number: 2,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+    { 
+      number: 3,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+    { 
+      number: 4,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+    { 
+      number: 5,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+    { 
+      number: 6,
+      date: '01/10/21',
+      therapist: 'gdrgsdfgsdfggsgf',
+      id: 'SFH862',
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
